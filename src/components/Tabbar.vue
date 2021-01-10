eb42da2d981c56ebfeae48320f636bd0a8940c53
grea-t <1193976273@qq.com>
2021-01-10 15:00:02 +0800

    tabbar

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
+  <nav>
     <ul>
-      <router-link to="/film" tag="li" active-class="clickActive">电影</router-link>
-      <router-link to="/cinema" tag="li" active-class="clickActive">影院</router-link>
-      <router-link to="/center" tag="li" active-class="clickActive">我的</router-link>
+      <router-link to="/film" tag="li" active-class="clickActive">
+        <i class="iconfont icon-film"></i>
+        <span>电影</span>
+      </router-link>
+      <router-link to="/cinema" tag="li" active-class="clickActive">
+        <i class="iconfont icon-cinema"></i>
+        <span>影院</span>
+      </router-link>
+      <router-link to="/center" tag="li" active-class="clickActive">
+        <i class="iconfont icon-center"></i>
+        <span>我的</span>
+      </router-link>
     </ul>
-  </div>
+  </nav>
 </template>
 
 <script>
@@ -15,14 +24,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  ul {
-    display: flex;
+  nav {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    line-height: 25px;
+    text-align: center;
+    background: white;
 
-    li {
-      flex: 1;
+    ul {
+      display: flex;
+
+      li {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        i{
+          font-size: 20px;
+        }
+        span{
+          font-size: 13px;
+        }
+      }
     }
   }
-  .clickActive{
+
+  .clickActive {
     color: red;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Center from '../views/Center'
 import Film from '../views/Film'
 import Cinema from '../views/Cinema'
 import ComingSoon from '../views/film/ComingSoon'
 import NowPlaying from '../views/film/NowPlaying'
 import Detail from '../views/Detail'
-import Login from '../views/Login'
 
 Vue.use(VueRouter)
 const routes = [
@@ -30,10 +28,10 @@ const routes = [
     component: Cinema
   }, {
     path: '/center',
-    component: Center
+    component: () => import(/* webpackChunkName:"componentGroup" */ '../views/Center.vue')
   }, {
     path: '/login',
-    component: Login
+    component: () => import(/* webpackChunkName:"componentGroup" */ '../views/Login.vue')
   }, {
     path: '/detail/:myid',
     component: Detail
