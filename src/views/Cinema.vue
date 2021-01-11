ca99c0de782ef65004ea6bc356eceb26ece785b0
grea-t <1193976273@qq.com>
2021-01-11 15:26:30 +0800

    betterscroll

--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    cinema
+  <div class="cinema" :style="{height:height}">
+    <ul>
+      <li v-for="data in cinemaList" :key="data.cinemaId">
+        <div>{{data.name}}</div>
+        <div>{{data.address}}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import http from '../util/http'
+import BetterScroll from 'better-scroll'
+
 export default {
-  name: 'Cinema'
+  data () {
+    return {
+      cinemaList: [],
+      height: 0
+    }
+  },
+  mounted () {
+    this.height = document.documentElement.clientHeight - 50 + 'px'
+    http({
+      url: '/gateway?cityId=110100&ticketFlag=1&k=6468565',
+      headers: {
+        'X-Host': 'mall.film-ticket.cinema.list'
+      }
+    }).then(res => {
+      this.cinemaList = res.data.data.cinemas
+      this.$nextTick(() => {
+        new BetterScroll('.cinema', {
+          scrollbar: {
+            fade: true
+          }
+        })
+      })
+    })
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .cinema{
+    overflow: hidden;
+    position: relative; // 修正滚动条位置
+  }
+  li {
+    padding: 15px;
+  }
 </style>
