0cf314182a97bec30d982f2fe2cd714ecff918c3
grea-t <1193976273@qq.com>
2021-01-13 17:46:15 +0800

    vuex 持久化

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import Vuex from 'vuex'
 import CityModule from './module/CityModule'
 import CinemaModule from './module/CinemaModule'
 import TabbarModule from './module/TabbarModule'
+import persistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [persistedState()],
   state: {},
   mutations: {},
   actions: {},
